471ddf9e3b0657623d73964e825da0b70b3bbb68
tminhein <tminhein@gmail.com>
2020-07-08 11:37:25 +0100

    removed funciton2

--- FunctionApp1/Functions/Function2.cs
+++ FunctionApp1/Functions/Function2.cs
@@ -10,24 +10,24 @@ using Newtonsoft.Json;
 
 namespace FunctionApp1.Functions
 {
-    public static class Function2
-    {
-        [FunctionName("Function2")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+    // public static class Function2
+    // {
+    //     [FunctionName("Function2")]
+    //     public static async Task<IActionResult> Run(
+    //         [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+    //         ILogger log)
+    //     {
+    //         log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+    //         string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+    //         string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+    //         dynamic data = JsonConvert.DeserializeObject(requestBody);
+    //         name = name ?? data?.name;
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
-        }
-    }
+    //         return name != null
+    //             ? (ActionResult)new OkObjectResult($"Hello, {name}")
+    //             : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+    //     }
+    // }
 }
