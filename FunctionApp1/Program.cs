c3dfeedfa39543fd44fbf6a2a7ad862d66aa104b
tminhein <tminhein@gmail.com>
2020-07-08 11:05:55 +0100

    added function2

---
+++ FunctionApp1/Program.cs
@@ -0,0 +1,26 @@
+using System.IO;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.Internal;
+
+[assembly: FunctionsStartup(typeof(FunctionApp1.Program))]
+
+namespace FunctionApp1
+{
+    public class Program : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+		{
+			var hostingEnvironment = new HostingEnvironment
+			{
+				ContentRootPath = Directory.GetCurrentDirectory(),
+				ApplicationName = "Operation Functions",
+				EnvironmentName = "Ring0"
+			};
+
+			builder.Services.Add(new ServiceDescriptor(typeof(IHostEnvironment), hostingEnvironment));	
+		}
+    }
+}
